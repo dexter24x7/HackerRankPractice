6e09428f8c4c9d34a07697b7ecb7cb0244827add
dexter24x7 <117462852+dexter24x7@users.noreply.github.com>
2022-12-23 10:17:45 +0530

    New repo

---
+++ src/TagContentExtractor.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.util.regex.*;
+
+    public class TagContentExtractor{
+        public static void main(String[] args){
+
+            Scanner in = new Scanner(System.in);
+            int testCases = Integer.parseInt(in.nextLine());
+            while(testCases>0){
+                String line = in.nextLine();
+                //Write your code here
+                int flag=0;
+                Pattern pat = Pattern.compile("<(.+)>((?:\\w|\\s|\\W&&[^<>])+)</\\1>");
+                Matcher mat = pat.matcher(line);
+                while(mat.find()) {
+                    System.out.println(mat.group(2));
+                    flag++;
+                }
+                if (flag==0) System.out.print("None");
+                testCases--;
+            }
+        }
+}
